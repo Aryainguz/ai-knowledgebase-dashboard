27642a5360cb415d65ee7279aa47f6c1a5088c71
Aryainguz <aryaninguz369@gmail.com>
2024-07-31 00:50:59 +0530

    Fix typo in dashboard link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
    <div className="flex justify-center items-center mt-44 text-2xl">
-    Go to <Link href={"/dahsboard"}>Dashboard</Link>
+    Go to <Link href={"/dashboard"}>Dashboard</Link>
    </div>
   )
 }
