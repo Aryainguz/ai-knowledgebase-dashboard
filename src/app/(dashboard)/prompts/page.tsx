f6abe533fce14053bc50ff1642b5a6ffe2253996
Aryainguz <aryaninguz369@gmail.com>
2024-08-01 18:31:47 +0530

    feat: modified dashboard prompts component and removed breadcrumbs nav

--- src/app/(dashboard)/prompts/page.tsx
+++ src/app/(dashboard)/prompts/page.tsx
@@ -1,38 +1,10 @@
-import Link from "next/link";
-
 import { ContentLayout } from "@/components/admin-panel/content-layout";
 import Prompts from "@/components/prompts";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from "@/components/ui/breadcrumb";
+
 
 export default function PromptsPage() {
   return (
     <ContentLayout title="Prompts">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="/">Home</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="/dashboard">Dashboard</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Prompts</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
       <Prompts />
     </ContentLayout>
   );
