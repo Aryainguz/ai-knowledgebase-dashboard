ff48ae030ab1f166cd249014699a71cc8fad91fb
Aryainguz <aryaninguz369@gmail.com>
2024-08-02 09:22:23 +0530

    feat: Improve page layout and add button to navigate to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
-   <div className="flex justify-center items-center mt-44 text-2xl">
-    Go to <Link href={"/dashboard"}>Dashboard</Link>
-   </div>
+    <div className="flex justify-center items-center mt-44 text-2xl">
+      <Link href={'/dashboard'}>
+        <Button className="bg-[#0f172a] hover:bg-slate-700 hover:text-white text-sm mt-2 w-full text-center dark:text-white">
+          Go To Dashboard
+        </Button>
+      </Link>
+
+    </div>
   )
 }
 
